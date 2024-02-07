9f5910d493c43a7dccec13c1bd0a4bcab8036d52
Dhruv <Dhrvthpr@gmail.com>
2024-02-07 12:19:05 +0530

    Added frontend changes from dhruv

---
+++ src/components/ModalSend.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ModalSend = ({props}) => {
+  return (
+    <div className="h-[50%] w-[60%] bg-white rounded-3xl flex flex-col justify-center items-center">
+        <div className=" text-black font-bold text-3xl" > Send Money</div>
+    </div>
+  )
+}
+
+export default ModalSend
--- src/components/SendMoneyBar.jsx
+++ src/components/SendMoneyBar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import {useNavigate} from "react-router-dom"
 
-const SendMoneyBar = (index,props) => {
+const SendMoneyBar = ({index,props}) => {
+  const navigate = useNavigate();
   return (
     <div className="h-[5rem] w-[100%] grid grid-cols-10  rounded-3xl ">
 
-        <div className="col-span-1  flex justify-center items-center"><div className="h-[4rem] rounded-full w-[4rem] bg-gray-300 items-center flex justify-center text-black">1</div></div>
+        <div className="col-span-1  flex justify-center items-center"><div className="h-[4rem] rounded-full w-[4rem] bg-gray-300 items-center flex justify-center text-black">{index}</div></div>
 
-        <div className="col-span-2  flex justify-start "><div className="   flex  my-auto text-lg font-semibold">User name</div></div>
+        <div className="col-span-2  flex justify-start "><div className="   flex  my-auto text-lg font-semibold">{props.name}
+        <p>{props.id}</p>
+        </div></div>
 
-        <div className="col-span-7  flex justify-end items-center"><div className=" bg-black items-center flex justify-center h-[3.5rem] w-[8rem] rounded-2xl text-white hover:scale-110 transition-transform ">Send money</div></div>
+        <div className="col-span-7  flex justify-end items-center"><div className=" bg-black items-center flex justify-center h-[3.5rem] w-[8rem] rounded-2xl text-white hover:scale-110 transition-transform " onClick={()=>{
+          navigate("/modal")
+        }}>Send money</div></div>
 
     </div>
   )
